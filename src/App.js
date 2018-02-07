b8393c03ffb0e9484d3b41206d3a88d4cd5704cc
Dhayanidhi Jayaraman <dhayajr@dhayanidhis-mbp.local>
2018-02-07 16:43:28 -0500

    Deploy new contract implementation done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import ContractNew from './modules/contract-new';
 import AMNew from './modules/am-new';
 import { web3 } from './web3';
 
@@ -15,10 +17,17 @@ class App extends Component {
             {web3.isConnected() && <p className = "lead">Heroku Smart Contract Administration</p>}
 
             {!web3.isConnected() && <p className = "lead">Initiating the smart contract instance</p>}
+
           </div>
         </div>
 
-        <AMNew />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={ContractNew} />
+                <Route exact path='/car' component={AMNew} />
+            </Switch>
+        </BrowserRouter>
+
       </div>
 
     );
--- src/web3.js
+++ src/web3.js
@@ -4,6 +4,7 @@ const blockChainLocation = 'http://localhost:8545';
 
 export const web3 = new Web3(new Web3.providers.HttpProvider(blockChainLocation));
 
+export const web3Manager = Web3;
 
 export const web3Connection = {
     retry: () => {
