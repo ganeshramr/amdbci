cc833d22a07197eebce872bc234950b5e8751eae
ganehsramr <mailganesh@gmail.com>
2018-03-05 14:49:24 -0500

    auto url

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000,
     app = express();
 
 app.use(express.static(path.join(__dirname, 'build')))
-
+console.log("process.env.BLOCKCHAIN_URL"+process.env.BLOCKCHAIN_URL);
 app.listen(PORT)
--- src/web3.js
+++ src/web3.js
@@ -2,13 +2,13 @@ import Web3 from 'web3';
 
 const blockChainLocation = 'http://private-blockchain.herokuapp.com/';
 
-export const web3 = new Web3(new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_URL));
+export const web3 = new Web3(new Web3.providers.HttpProvider(blockChainLocation));
 
 export const web3Manager = Web3;
 
 export const web3Connection = {
     retry: () => {
-        web3.setProvider(new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_URL));
+        web3.setProvider(new Web3.providers.HttpProvider(blockChainLocation));
         console.log('Web3 Connection Retry', web3);
     }
 }
